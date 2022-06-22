b31372d49346ef21d7439d922278e04c7579e876
panopticonism <panopticonism@gmail.com>
2022-06-22 17:16:03 +0200

    Burger menu fixed.

--- frontend/components/Header/Header.jsx
+++ frontend/components/Header/Header.jsx
@@ -1,28 +1,40 @@
 import styles from "./Header.module.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+  console.log(isOpen);
   return (
     <header className={styles.header}>
       <img src="/logo.svg" alt="panopticonism-logo" />
+      <div
+        className={`${styles.closeBox} ${isOpen ? styles.show : ""}`}
+        onClick={toggle}
+      >
+        <span className={styles.closeBtn}></span>
+      </div>
       <nav>
-        <input type="checkbox" id={styles.check} />
-        <label htmlFor="check" className={styles.checkBtn}>
-          <span></span>
-        </label>
-        <ul className={styles.list}>
+        <ul className={`${styles.list} ${isOpen ? styles.show : ""}`}>
           <li>
-            <a href="#steps">Competences</a>
+            <a href="#steps" onClick={toggle}>
+              Competences
+            </a>
           </li>
           <li>
-            <a href="#experience">Experience</a>
+            <a href="#experience" onClick={toggle}>
+              Experience
+            </a>
           </li>
           <li>
-            <a href="#projects">Portfolio</a>
+            <a href="#projects" onClick={toggle}>
+              Portfolio
+            </a>
           </li>
           <li className={styles.hire_me}>
-            <a href="#hire_me">Hire me</a>
+            <a href="#hire_me" onClick={toggle}>
+              Hire me
+            </a>
           </li>
         </ul>
       </nav>
