9fe192e12869412eaafb6f3543922511c78d7601
panopticonism <panopticonism@gmail.com>
2022-06-22 18:23:02 +0200

    Test: header removed.

--- frontend/components/index.js
+++ frontend/components/index.js
@@ -1,5 +1,4 @@
 export { default as LoadingScreen } from "./Loading/Loading";
-export { default as Header } from "./Header/Header";
 export { default as Socials } from "./Socials/Socials";
 export { default as ProjectsCTA } from "./Projects/Projects";
 export { default as Footer } from "./Footer/Footer";
