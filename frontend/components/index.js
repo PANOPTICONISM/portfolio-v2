d2a20c61218aba9e677e3dcdcb728742bafb890c
panopticonism <panopticonism@gmail.com>
2022-06-22 18:29:44 +0200

    Revert "Test: header removed."
    This reverts commit beb8fefd806148a3e3dd271d98db792ed19a4952.

--- frontend/components/index.js
+++ frontend/components/index.js
@@ -1,4 +1,5 @@
 export { default as LoadingScreen } from "./Loading/Loading";
+export { default as Header } from "./Header/Header";
 export { default as Socials } from "./Socials/Socials";
 export { default as ProjectsCTA } from "./Projects/Projects";
 export { default as Footer } from "./Footer/Footer";
