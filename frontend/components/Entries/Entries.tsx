0eaafd46808f6af327a29c8a6b9992fc01a43dac
panopticonism <panopticonism@gmail.com>
2022-12-21 00:21:35 +0100

    Entries type added.

--- frontend/components/Entries/Entries.tsx
+++ frontend/components/Entries/Entries.tsx
@@ -2,13 +2,24 @@ import Link from 'next/link';
 import React from 'react';
 import styles from "./Entries.module.css";
 
-export const Entries = ({ posts }) => {
+type PostProps = {
+    id: string,
+    properties: {
+        Name: {
+            title: {
+                plain_text: string,
+            }[]
+        }
+    }
+}[];
+
+export const Entries = ({ posts }: { posts: PostProps }) => {
     return (
         <section className={styles.entries}>
             {posts.map((entry) =>
                 <article key={entry.id}>
                     <h2>{entry.properties.Name.title[0].plain_text}</h2>
-                    <Link href={`/blog/${entry.id}`}>Read post →</Link>
+                    <Link href={`/blog/${entry.id}`}>Read article →</Link>
                 </article>
             )}
         </section>
