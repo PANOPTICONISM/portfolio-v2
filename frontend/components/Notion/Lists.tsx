afc3b5ccec37a017ecec149338ac44ff27dfc68b
panopticonism <panopticonism@gmail.com>
2023-01-26 17:06:14 +0100

    Minor styling fixes.

--- frontend/components/Notion/Lists.tsx
+++ frontend/components/Notion/Lists.tsx
@@ -1,8 +1,9 @@
-import { TextProps } from "./types"
+import { TextProps } from "./types";
+import styles from "./Blocks.module.css";
 
 export const BulletedList = ({ text }: { text: TextProps }) => {
     return (
-        <ul>
+        <ul className={styles.list}>
             {text.map((value, index) =>
                 <li key={index}>
                     {value.text.link ? <a href={value.text.link.url}>{value.text.content}</a> : value.text.content}
