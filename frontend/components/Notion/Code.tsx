7366b9aa368bb599ac86492fa9608bbf60cc89ae
panopticonism <panopticonism@gmail.com>
2023-03-30 23:17:31 +0200

    Code syntax highlighted.

--- frontend/components/Notion/Code.tsx
+++ frontend/components/Notion/Code.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import styles from "./styles.module.css";
+import Prism from "prismjs";
 
-export const Code = ({ content }: { content: string }) => {
+export const Code = ({ content, language }: { content: string, language: string }) => {
+
+    React.useEffect(() => {
+        const highlightDynamically = async () => {
+            if (typeof window !== "undefined") {
+                Prism.highlightAll();
+                await import(`prismjs/components/prism-${language}`);
+            }
+        }
+        highlightDynamically();
+    }, [language]);
+
     return (
         <section className={styles.code}>
             <pre>
-                <code>
+                <code className={`language-javascript`}>
                     {content}
                 </code>
             </pre>
