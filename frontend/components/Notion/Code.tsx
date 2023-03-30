6e9b526b60b4184eeaf86f8a6db94252be7e21d8
panopticonism <panopticonism@gmail.com>
2023-03-31 00:05:35 +0200

    Line numbers.

--- frontend/components/Notion/Code.tsx
+++ frontend/components/Notion/Code.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styles from "./styles.module.css";
 import Prism from "prismjs";
+import 'prismjs/plugins/line-numbers/prism-line-numbers.js'
+import 'prismjs/plugins/line-numbers/prism-line-numbers.css'
 
 export const Code = ({ content, language }: { content: string, language: string }) => {
 
@@ -16,8 +18,8 @@ export const Code = ({ content, language }: { content: string, language: string 
 
     return (
         <section className={styles.code}>
-            <pre>
-                <code className={`language-javascript`}>
+            <pre className='line-numbers'>
+                <code className={`language-${language}`}>
                     {content}
                 </code>
             </pre>
