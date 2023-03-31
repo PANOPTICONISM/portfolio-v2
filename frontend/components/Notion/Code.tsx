bfbc3052cca2d1820c04c9f07b600b4505228a26
panopticonism <panopticonism@gmail.com>
2023-03-31 20:58:25 +0200

    Syntax highlight library updated.

--- frontend/components/Notion/Code.tsx
+++ frontend/components/Notion/Code.tsx
@@ -1,27 +1,15 @@
 import React from 'react';
 import styles from "./styles.module.css";
-import Prism from "prismjs";
-import 'prismjs/plugins/line-numbers/prism-line-numbers.js';
-import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { coldarkDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 export const Code = ({ content, language }: { content: string, language: string }) => {
 
-    React.useEffect(() => {
-        const highlightDynamically = async () => {
-            if (typeof window !== "undefined") {
-                Prism.highlightAll();
-            }
-        }
-        highlightDynamically();
-    }, [language]);
-
     return (
         <section className={styles.code}>
-            <pre className='line-numbers copy-to-clipboard'>
-                <code className={`language-${language}`} data-prismjs-copy="Copy the C snippet!">
-                    {content}
-                </code>
-            </pre>
+            <SyntaxHighlighter language="javascript" style={coldarkDark} showLineNumbers>
+                {content}
+            </SyntaxHighlighter>
         </section>
     )
 }
