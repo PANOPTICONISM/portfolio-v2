369811fe331826bf26a5aab6e135ae4364025cde
panopticonism <panopticonism@gmail.com>
2023-06-22 00:41:45 +0200

    Tech reorganised.

--- frontend/components/homepage/Skills/Tech.tsx
+++ frontend/components/homepage/Skills/Tech.tsx
@@ -1,34 +1,51 @@
 import styles from "./Tech.module.css";
-import { MouseEvent, SetStateAction, useState } from "react";
-import { TechProps } from "./types";
+import { useState } from "react";
+import { SkillProps, TechProps } from "./types";
+
+const Skill = ({ fields }: { fields: SkillProps }) => {
+  return (
+    <li key={fields.id}>
+      <img src={fields?.image?.fields?.file?.url} alt={fields?.title} />
+      <p>{fields.title}</p>
+    </li>
+  );
+};
 
 const TechTabs = ({ skills }: TechProps) => {
-  const usedCategories = skills.items.map((skill) => skill.metadata.tags?.[0]?.sys?.id);
+  const usedCategories = skills.items.map(
+    (skill) => skill.metadata.tags?.[0]?.sys?.id
+  );
   const categories = [...new Set(usedCategories)];
+
   const [activeTab, setActiveTab] = useState(categories[0]);
+
+  const currentSkills = skills.items.filter(
+    (skill) => activeTab === skill.metadata.tags?.[0]?.sys?.id
+  );
+
   const switchTab = (e: any) => {
     setActiveTab(e.target.textContent);
-  }
+  };
 
   return (
     <section className={styles.tech}>
       <h1 className={styles.heading}>Toolshed</h1>
       <div className={styles.container}>
         <ul className={styles.filters}>
-          {categories.map((category) =>
-            <li onClick={switchTab} key={category} className={activeTab === category ? styles.active : ""}>{category}</li>
-          )}
+          {categories.map((category) => (
+            <li
+              onClick={switchTab}
+              key={category}
+              className={activeTab === category ? styles.active : ""}
+            >
+              {category}
+            </li>
+          ))}
         </ul>
         <ul className={styles.stack}>
-          {skills.items.map((skill) =>
-            (activeTab === skill.metadata.tags?.[0]?.sys?.id) && <li key={skill.fields.id}>
-              <img
-                src={skill?.fields?.image?.fields?.file?.url}
-                alt={skill?.fields?.title}
-              />
-              <p>{skill.fields.title}</p>
-            </li>
-          )}
+          {currentSkills.map((skill) => (
+            <Skill key={skill.fields.id} fields={skill.fields} />
+          ))}
         </ul>
       </div>
     </section>
