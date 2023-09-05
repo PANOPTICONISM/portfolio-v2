8a24cd8e0d188893e1a1dc3ecf3075c2b1df7ad8
PANOPTICONISM <68908892+PANOPTICONISM@users.noreply.github.com>
2023-09-05 23:49:40 +0200

    Filters removed.

--- frontend/components/homepage/Posts/Posts.tsx
+++ frontend/components/homepage/Posts/Posts.tsx
@@ -1,6 +1,6 @@
 import styles from "./Posts.module.css";
 import Post from "./Post/Post";
-import { useState, forwardRef } from "react";
+import { forwardRef } from "react";
 import { ArticleFieldsProps } from "./types";
 
 interface ProjectsProps {
@@ -9,49 +9,13 @@ interface ProjectsProps {
 
 const Posts = forwardRef<HTMLInputElement, ProjectsProps>(
   ({ projects }, ref) => {
-    const removeFirstPost = projects?.slice(1);
-
-    const [filteredProjects, setFilteredProjects] = useState(removeFirstPost);
-    const [activeButton, setActiveButton] = useState("all");
-
-    const categories = ["all", "design", "development"];
-
-    const clickedFilter = (e: any) => {
-      setActiveButton(e.target.textContent);
-
-      const trigger = e.target.textContent;
-      let result: any[] = [];
-
-      removeFirstPost.map((project) => {
-        return project?.fields?.category?.filter((cat) => {
-          if (trigger === cat) {
-            result.push(project);
-          }
-          return false;
-        });
-      });
-
-      setFilteredProjects(result);
-    };
+    const allProjectsButRecent = projects?.slice(1);
 
     return (
       <section id="projects" className={styles.projects} ref={ref}>
         <h1>Portfolio</h1>
-        <div className={styles.filters}>
-          <ul>
-            {categories?.map((nameCat, index) => (
-              <li
-                onClick={(e) => clickedFilter(e)}
-                key={index}
-                className={nameCat === activeButton ? styles.active : ""}
-              >
-                {nameCat}
-              </li>
-            ))}
-          </ul>
-        </div>
         <div className={styles.portfolio}>
-          {filteredProjects?.map((article: any) => (
+          {allProjectsButRecent.map((article: any) => (
             <Post article={article} key={article?.sys?.id} />
           ))}
         </div>
