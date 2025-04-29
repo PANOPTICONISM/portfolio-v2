7a3b1acf077e56e34a2f42f07d17b7e448692274
MAL <68908892+PANOPTICONISM@users.noreply.github.com>
2025-04-30 00:10:19 +0200

    File naming + update to toolshed visuals.

--- components/homepage/index.ts
+++ components/homepage/index.ts
@@ -1,5 +1,5 @@
 export { default as Introduction } from "./Introduction/Introduction";
 export { default as Steps } from "./Steps/Steps";
-export { default as TechTabs } from "./Skills/Tech";
+export { default as Tech } from "./Tech/Tech";
 export { default as Recent } from "./Recent/Recent";
 export { default as Posts } from "./Posts/Posts";
