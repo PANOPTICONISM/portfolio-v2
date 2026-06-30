1b7ba11491520ef6193c70426298fc8321d66740
MAL <68908892+PANOPTICONISM@users.noreply.github.com>
2026-06-30 22:21:54 +0200

    feat: uninstalled posthog + upgraded next.

--- instrumentation-client.ts
+++
@@ -1,8 +0,0 @@
-import posthog from "posthog-js";
-
-posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-  api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-  defaults: "2026-01-30",
-  persistence: "memory",
-  disable_session_recording: true,
-});
