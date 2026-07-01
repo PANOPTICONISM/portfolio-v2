887da4467d4b94938bdf4881a4e5712fab4fab74
MAL <68908892+PANOPTICONISM@users.noreply.github.com>
2026-07-01 22:31:09 +0200

    fix: react-syntax-highlighter `require()` error.

--- components/Notion/Code.tsx
+++ components/Notion/Code.tsx
@@ -1,34 +1,57 @@
-import React from 'react';
+import React from "react";
+import dynamic from "next/dynamic";
 import styles from "./Notion.module.css";
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { coldarkDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faClone, faSquareCheck } from '@fortawesome/free-regular-svg-icons';
+import { coldarkDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClone, faSquareCheck } from "@fortawesome/free-regular-svg-icons";
 
-export const Code = ({ content, language, allowCopy }: { content: string, language: string, allowCopy: boolean }) => {
-    const [copied, setCopied] = React.useState(false);
+const SyntaxHighlighter = dynamic(
+  () => import("react-syntax-highlighter").then((mod) => mod.Prism),
+  { ssr: false, loading: () => null },
+);
 
-    React.useEffect(() => {
-        const timeout = setTimeout(() => setCopied(false), 2000);
+export const Code = ({
+  content,
+  language,
+  allowCopy,
+}: {
+  content: string;
+  language: string;
+  allowCopy: boolean;
+}) => {
+  const [copied, setCopied] = React.useState(false);
 
-        return () => clearTimeout(timeout);
-    }, [copied]);
+  React.useEffect(() => {
+    const timeout = setTimeout(() => setCopied(false), 2000);
 
-    const handleCopy = async () => {
-        await navigator.clipboard.writeText(content);
-        setCopied(true);
-    };
+    return () => clearTimeout(timeout);
+  }, [copied]);
 
-    return (
-        <div className={styles.code}>
-            {allowCopy ? (
-                <div className={styles.copyBtn} onClick={handleCopy}>
-                    {copied ? <FontAwesomeIcon icon={faSquareCheck} color='green' /> : <FontAwesomeIcon icon={faClone} color='white' />}
-                </div>
-            ) : null}
-            <SyntaxHighlighter language={language} style={coldarkDark} showLineNumbers showInlineLineNumbers lineNumberContainerStyle={{ left: '2px' }}>
-                {content}
-            </SyntaxHighlighter>
+  const handleCopy = async () => {
+    await navigator.clipboard.writeText(content);
+    setCopied(true);
+  };
+
+  return (
+    <div className={styles.code}>
+      {allowCopy ? (
+        <div className={styles.copyBtn} onClick={handleCopy}>
+          {copied ? (
+            <FontAwesomeIcon icon={faSquareCheck} color="green" />
+          ) : (
+            <FontAwesomeIcon icon={faClone} color="white" />
+          )}
         </div>
-    )
-}
+      ) : null}
+      <SyntaxHighlighter
+        language={language}
+        style={coldarkDark}
+        showLineNumbers
+        showInlineLineNumbers
+        lineNumberContainerStyle={{ left: "2px" }}
+      >
+        {content}
+      </SyntaxHighlighter>
+    </div>
+  );
+};
